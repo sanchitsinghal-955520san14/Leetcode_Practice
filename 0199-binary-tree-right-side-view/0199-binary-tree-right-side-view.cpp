b151d277108a88a4e7e2b9acdc91827e0c375be7
sanchit agrawal <2k23.csai2312714@gmail.com>
2026-09-24 18:42:52 +0530

    Time: 0 ms (100%), Space: 15 MB (40.23%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,33 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class SolutionA {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<vector<int>> temp;
+//         vector<int> ans;
+//         queue<TreeNode*> q;
+//         if(root == NULL){
+//             return ans;
+//         }
+//         q.push(root);
+//         while(!q.empty()) {
+//             int size = q.size();
+//             vector<int> level;
+//             for(int i = 0; i < size; i++) {
+//                 TreeNode* curr = q.front();
+//                 q.pop();
+//                 level.push_back(curr->val);
+//                 if(curr->left)
+//                     q.push(curr->left);
+//                 if(curr->right)
+//                     q.push(curr->right);
+//             }
+//             temp.push_back(level);
+//         }
+//         for(int i = 0; i < temp.size(); i++) {
+//             ans.push_back(temp[i].back());
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<vector<int>> temp;
-        vector<int> ans;
-        queue<TreeNode*> q;
-        if(root == NULL){
-            return ans;
-        }
-        q.push(root);
-        while(!q.empty()) {
-            int size = q.size();
-            vector<int> level;
-            for(int i = 0; i < size; i++) {
-                TreeNode* curr = q.front();
-                q.pop();
-                level.push_back(curr->val);
-                if(curr->left)
-                    q.push(curr->left);
-                if(curr->right)
-                    q.push(curr->right);
+      vector<int> ans;
+      queue<TreeNode* >q;
+      if(root==NULL)return ans;
+      q.push(root);
+      while(!q.empty()){
+        int size=q.size();
+        for(int i=0;i<size;i++){
+            TreeNode* curr=q.front();
+            q.pop();
+            if(i==size-1){
+                ans.push_back(curr->val);
             }
-            temp.push_back(level);
+            if(curr->left)q.push(curr->left);
+            if(curr->right)q.push(curr->right);
         }
-        for(int i = 0; i < temp.size(); i++) {
-            ans.push_back(temp[i].back());
-        }
-        return ans;
+      }
+      return ans;
     }
 };
